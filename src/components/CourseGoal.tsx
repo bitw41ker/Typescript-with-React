3ef518210c64ab4cc6cd77c15749acf428b0b6bb
bitw41ker <bitw41ker@gmail.com>
2024-03-04 21:56:08 +0200

    Add goal delete logic

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -2,18 +2,25 @@ import { type ReactNode } from 'react';
 // import { type FC, type ReactNode } from 'react';
 
 type CourseGoalProps = {
+  id: number;
   title: string;
+  onDelete: (id: number) => void;
   children?: ReactNode;
 };
 
-export default function CourseGoal({ title, children }: CourseGoalProps) {
+export default function CourseGoal({
+  id,
+  title,
+  children,
+  onDelete,
+}: CourseGoalProps) {
   return (
     <article>
       <div>
         <h2>{title}</h2>
         {children}
       </div>
-      <button>Delete</button>
+      <button onClick={() => onDelete(id)}>Delete</button>
     </article>
   );
 }
