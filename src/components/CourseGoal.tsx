b64d490a2d8627d94a64c464a26d643576648657
bitw41ker <bitw41ker@gmail.com>
2024-03-04 15:19:21 +0200

    Add alternative way of defining functional component as a value

--- src/components/CourseGoal.tsx
+++ src/components/CourseGoal.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from 'react';
+// import { type FC, type ReactNode } from 'react';
 
 type CourseGoalProps = {
   title: string;
@@ -16,3 +17,17 @@ export default function CourseGoal({ title, children }: CourseGoalProps) {
     </article>
   );
 }
+
+// const CourseGoal: FC<CourseGoalProps> = ({ title, children }) => {
+//   return (
+//     <article>
+//       <div>
+//         <h2>{title}</h2>
+//         {children}
+//       </div>
+//       <button>Delete</button>
+//     </article>
+//   );
+// };
+
+// export default CourseGoal;
