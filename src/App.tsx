59486fa726668e6c55992cb4111044b3154d3f3d
bitw41ker <bitw41ker@gmail.com>
2024-03-04 14:32:50 +0200

    Define a type for props with "children"

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,9 @@ import CourseGoal from './components/CourseGoal.tsx';
 export default function App() {
   return (
     <main>
-      <CourseGoal
-        title="React + TS"
-        description="Learn Typescript with React"
-      />
+      <CourseGoal title="React + TS">
+        <p>Learn Typescript with React</p>
+      </CourseGoal>
     </main>
   );
 }
