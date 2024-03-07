cd23e05a0bbf4baad8028cb391c6a33b03e44322
bitw41ker <bitw41ker@gmail.com>
2024-03-07 15:27:23 +0200

    Make Polymorphic component generic

--- advanced-components/src/components/Container.tsx
+++ advanced-components/src/components/Container.tsx
@@ -1,9 +1,18 @@
-import { type ElementType } from 'react';
+import {
+  type ComponentPropsWithoutRef,
+  type ElementType,
+  type ReactNode,
+} from 'react';
 
-type ContainerProps = {
+type ContainerProps<T extends ElementType> = {
   as: ElementType;
-};
+  children: ReactNode;
+} & ComponentPropsWithoutRef<T>;
 
-export default function Container({ as: Component }: ContainerProps) {
-  return <Component />;
+export default function Container<C extends ElementType>({
+  as: Component,
+  children,
+  ...props
+}: ContainerProps<C>) {
+  return <Component {...props}>{children}</Component>;
 }
