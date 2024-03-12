b742b70dd08fab50ddf31a4d1ab486b4fa3ab3d6
bitw41ker <bitw41ker@gmail.com>
2024-03-12 17:59:33 +0200

    Add logic to clear the form by using ref

--- advanced-components/src/components/Form.tsx
+++ advanced-components/src/components/Form.tsx
@@ -1,20 +1,22 @@
-import { type ComponentPropsWithoutRef, type FormEvent } from 'react';
+import { type ComponentPropsWithoutRef, type FormEvent, useRef } from 'react';
 
 type FormProps = ComponentPropsWithoutRef<'form'> & {
   onSubmit: (value: unknown) => void;
 };
 
 export default function Form({ onSubmit, children, ...otherProps }: FormProps) {
+  const formRef = useRef<HTMLFormElement>(null);
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
     const data = Object.fromEntries(formData);
     onSubmit(data);
+    formRef.current?.reset();
   }
 
   return (
-    <form onSubmit={handleSubmit} {...otherProps}>
+    <form onSubmit={handleSubmit} {...otherProps} ref={formRef}>
       {children}
     </form>
   );
